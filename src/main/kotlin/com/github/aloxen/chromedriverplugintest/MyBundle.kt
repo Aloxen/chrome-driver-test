0277f362482ebff7481820fba83aff919296ea0b
GitHub Action <action@github.com>
2024-08-20 06:25:12 +0000

    Template cleanup

--- src/main/kotlin/com/github/aloxen/chromedriverplugintest/MyBundle.kt
+++ src/main/kotlin/com/github/aloxen/chromedriverplugintest/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.aloxen.chromedriverplugintest
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/aloxen/chromedriverplugintest/services/MyProjectService.kt
+++ src/main/kotlin/com/github/aloxen/chromedriverplugintest/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.aloxen.chromedriverplugintest.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.aloxen.chromedriverplugintest.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
